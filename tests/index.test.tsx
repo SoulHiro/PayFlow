6f7f87321e5791fd8a534aa15cd5fad4202f72ff
Victor <victormts.s1@gmail.com>
2026-07-06 17:34:47 -0300

    chore(test): set up vitest with jsdom and testing-library

---
+++ tests/index.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import { expect, test } from "vitest";
+import Home from "../src/app/page";
+
+test("Home", () => {
+  render(<Home />);
+  expect(
+    screen.getByRole("heading", { level: 1, name: "Hello World" }),
+  ).toBeDefined();
+});
